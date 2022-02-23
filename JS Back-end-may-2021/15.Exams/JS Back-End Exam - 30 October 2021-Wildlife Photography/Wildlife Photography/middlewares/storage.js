67c1b71fa77cc5d7162ade1b8c282e48d8c4c57f
Lyubomir Lyubomirov <Ljubo6@abv.bg>
2022-02-23 23:53:12 +0200

    delete

--- JS Back-end-may-2021/15.Exams/JS Back-End Exam - 30 October 2021-Wildlife Photography/Wildlife Photography/middlewares/storage.js
+++
@@ -1,9 +0,0 @@
-const postService = require('../services/post')
-module.exports = () => (req,res,next) => {
-
-    //TODO import and decorate services
-    req.storage ={
-        ...postService
-    }
-	next()
-}
